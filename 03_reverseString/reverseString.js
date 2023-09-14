383bb7f6cae8ba070737cf6081a01ccee6a4e642
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-14 14:17:16 +0200

    ex03 completed. passed all tests. for optimization, simplify by stringing multiple methods together, thus minimizing coded lines

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let arr = str.split("");
+    let output = arr.reverse().join("");
+    return output;
 };
+
+// console.log(reverseString("abc"));
 
 // Do not edit below this line
 module.exports = reverseString;
+ 
