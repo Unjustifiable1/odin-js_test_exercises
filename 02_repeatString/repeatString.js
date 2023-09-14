2c5cbb379e60796b8cb0804ca57a703664610af6
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-14 13:55:11 +0200

    ex2 completed. all tests passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num >= 0) {
+        let output = "";
+        for (let i = 0; i < num && i >= 0; i++) {
+            output += string;
+        }
+        return output;
+    } else return 'ERROR';
 };
 
 // Do not edit below this line
