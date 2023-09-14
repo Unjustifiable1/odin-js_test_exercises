7c09d0ab37ed7d3886f606d6893ac4af3b1739fa
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-14 15:25:31 +0200

    ex04 completed. all tests passed. array.includes() method used

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,20 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...theArgs) {
+    // console.log(arr);
+    // console.log(theArgs);
 
+    let output = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (theArgs.includes(arr[i]) == false) {
+            output.push(arr[i]);
+        }
+    }
+
+    console.log(output);
+    return output;
 };
+
+// removeFromArray([1, 2, 3, 4], 3, 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
