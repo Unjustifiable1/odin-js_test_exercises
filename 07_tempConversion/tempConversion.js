7c85ca98641c44e829bc3bee7f26de6122e5c333
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-15 11:10:23 +0200

    ex07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fart) {
+  // 5/9(F-32)
+  return parseFloat((5 / 9 * (fart - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cels) {
+  // C × (9/5) + 32
+  // return (cels * (9 / 5) + 32).toFixed(1);
+  return parseFloat((cels * (9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
