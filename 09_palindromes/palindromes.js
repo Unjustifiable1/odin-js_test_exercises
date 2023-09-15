00c5d91914c54f500d1b06fda118c107d00caf56
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-15 15:59:14 +0200

    ex09 completed

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let inputChars = str.split("");
+    let startChars = [];
+    let endChars = [];
 
+    for (let i = 0; i < inputChars.length; i++) {
+        if (inputChars[i].match("^[a-z0-9]+$")) {
+            startChars.push(inputChars[i]);
+            endChars.unshift(inputChars[i]);
+        }
+    }
+    return (startChars.toString() == endChars.toString());
 };
 
 // Do not edit below this line
