c7809c7887284d6b7046602ac67ecbf2671362de
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-18 11:46:41 +0200

    ex10 completed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
-};
+const fibonacci = function(pos) {
+    if (pos < 0) return 'OOPS';
+    pos *= 1;
+    let fibSeries = [0, 1, 1];
+    for (let i = 3; i <= pos; i++) {
+        fibSeries.push(fibSeries[i-2] + fibSeries[i-1]);
+    }
+    return fibSeries[fibSeries.length - 1];
+}
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+
+// series start as [0, 1, 1]
+
+// calc fibonacci up to requested position (n)
+
+// call requested position at series position e.g. series[n] 
