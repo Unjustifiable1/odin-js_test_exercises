c828871a8a14f7123e865a3673abb6228c25c136
Unjustifiable1 <cupidojustin7@gmail.com>
2023-09-18 13:14:43 +0200

    ex11 completed

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,25 @@
 const getTheTitles = function() {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ];
 
+    const titles = [];
+
+    for (let i = 0; i < books.length; i++) {
+        titles.push(books[i].title);
+    }
+
+    return titles;
 };
 
 // Do not edit below this line
 module.exports = getTheTitles;
+
+// console.log(getTheTitles());
